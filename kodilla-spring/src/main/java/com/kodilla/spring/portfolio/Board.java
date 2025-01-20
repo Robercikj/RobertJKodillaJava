4e72f719a3db8e221152dfcd5f15a0421d027360
Robert <robertjakubowski61@gmail.com>
2025-01-20 12:34:46 +0100

    13.3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.portfolio;
+
+public class Board {
+    private TaskList toDoList;
+    private TaskList inProgressList;
+    private TaskList doneList;
+
+    public Board(TaskList doneList, TaskList inProgressList, TaskList toDoList) {
+        this.doneList = doneList;
+        this.inProgressList = inProgressList;
+        this.toDoList = toDoList;
+    }
+
+    public TaskList getToDoList() {
+        return toDoList;
+    }
+
+    public TaskList getInProgressList() {
+        return inProgressList;
+    }
+
+    public TaskList getDoneList() {
+        return doneList;
+    }
+}
